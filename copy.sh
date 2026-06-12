e22b3ec01ad59ee900c83c0dbef6cafa9d27bffb
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-06-13 04:52:20 +0900

    Remove echo

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 for target in claude_utils claude-official-docs coding-rules hooks skills settings.json
 do
-    echo cp -r .claude/$target ~/.claude
+    cp -r .claude/$target ~/.claude
 done
 
 DIR_NOW=$(pwd)
